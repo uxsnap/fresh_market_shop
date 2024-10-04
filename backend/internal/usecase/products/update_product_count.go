7aad94acd489dc23a40273215870d43f89d6ca52
Балобанов Владислав <vbalobanov@zunami.ru>
2024-10-04 16:57:36 +0300

    feat(uc products): добавлены методы для работы с количеством продуктов

--- backend/internal/usecase/products/update_product_count.go
+++ backend/internal/usecase/products/update_product_count.go
@@ -28,3 +28,47 @@ func (uc *UseCaseProducts) UpdateProductCount(ctx context.Context, productUid uu
 
 	return nil
 }
+
+func (uc *UseCaseProducts) IncrementProductCount(ctx context.Context, productUid uuid.UUID, incValue int64) error {
+	log.Printf("ucProducts.IncrementProductCount product uid: %s, incValue: %d", productUid, incValue)
+
+	if err := uc.changeProductCount(ctx, productUid, incValue, true); err != nil {
+		log.Printf("failed to increment product(uid: %s) count:%v", productUid, err)
+		return errors.WithStack(err)
+	}
+	return nil
+}
+
+func (uc *UseCaseProducts) DecrementProductCount(ctx context.Context, productUid uuid.UUID, decValue int64) error {
+	log.Printf("ucProducts.DecrementProductCount product uid: %s, incValue: %d", productUid, decValue)
+
+	if err := uc.changeProductCount(ctx, productUid, decValue, false); err != nil {
+		log.Printf("failed to decrement product(uid: %s) count:%v", productUid, err)
+		return errors.WithStack(err)
+	}
+	return nil
+}
+
+func (uc *UseCaseProducts) changeProductCount(ctx context.Context, productUid uuid.UUID, value int64, inc bool) error {
+	return uc.txManager.NewPgTransaction().Execute(ctx, func(ctx context.Context) error {
+		pc, isFound, err := uc.productsRepository.GetProductCount(ctx, productUid)
+		if err != nil {
+			return err
+		}
+		if !isFound {
+			return errors.Errorf("product count with product uid %s not found", productUid)
+		}
+
+		var newCount int64
+		if inc {
+			newCount = pc + value
+		} else {
+			newCount = pc - value
+			if newCount < 0 {
+				return errors.New("new product count < 0")
+			}
+		}
+
+		return uc.productsRepository.UpdateProductCount(ctx, productUid, newCount)
+	})
+}
