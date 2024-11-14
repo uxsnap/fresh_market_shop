534560c73520d1774a6bcab0e1ea803d17a90802
Балобанов Владислав <vbalobanov@zunami.ru>
2024-11-14 20:40:45 +0300

    refactor auth subrouter

--- backend/internal/usecase/users/update_user.go
+++ backend/internal/usecase/users/update_user.go
@@ -27,22 +27,36 @@ func (uc *UseCaseUsers) UpdateUser(ctx context.Context, user entity.User) error 
 		)
 	}
 
-	if _, err = mail.ParseAddress(user.Email); err != nil {
-		return errorWrapper.NewError(
-			errorWrapper.UserEmailError, "неправильный формат email",
-		)
-	}
-
-	if len(user.FirstName) < 2 {
-		return errorWrapper.NewError(
-			errorWrapper.UserNameError, "длина имени и фамилии пользователя должна быть больше 1",
-		)
-	}
-
 	if time.Since(savedUser.UpdatedAt).Minutes() < 15 {
 		return errorWrapper.NewError(
 			errorWrapper.UserInfoError, "данные пользователя можно обновлять раз в 15 минут",
 		)
+	}
+
+	if len(user.Email) != 0 && user.Email != savedUser.Email {
+		if _, err = mail.ParseAddress(user.Email); err != nil {
+			return errorWrapper.NewError(
+				errorWrapper.UserEmailError, "неправильный формат email",
+			)
+		}
+		savedUser.Email = user.Email
+	}
+
+	// TODO: добавить проверку на занятость email
+
+	if len(user.FirstName) != 0 && user.FirstName != savedUser.FirstName {
+		if len(user.FirstName) < 2 {
+			return errorWrapper.NewError(
+				errorWrapper.UserNameError, "длина имени и фамилии пользователя должна быть больше 1",
+			)
+		}
+		savedUser.FirstName = user.FirstName
+	}
+	if len(user.LastName) != 0 && user.LastName != savedUser.LastName {
+		savedUser.LastName = user.LastName
+	}
+	if user.Birthday.Unix() != savedUser.Birthday.Unix() {
+		savedUser.Birthday = user.Birthday
 	}
 
 	if err := uc.usersRepository.UpdateUser(ctx, user); err != nil {
