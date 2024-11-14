e260fbc835c02448806b68ae51b3539ee2b1dc6d
Балобанов Владислав <vbalobanov@zunami.ru>
2024-11-14 19:39:52 +0300

    fix register: fix split req.Name

--- backend/internal/delivery/http/subrouters/auth/register.go
+++ backend/internal/delivery/http/subrouters/auth/register.go
@@ -27,8 +27,13 @@ func (h *AuthSubrouter) Register(w http.ResponseWriter, r *http.Request) {
 	}
 
 	nameSlice := strings.Split(req.Name, " ")
+	firstName := nameSlice[0]
+	lastName := ""
+	if len(nameSlice) == 2 {
+		lastName = nameSlice[1]
+	}
 
-	if len(nameSlice) == 0 || len(nameSlice[0]) < 2 {
+	if len(firstName) < 2 {
 		log.Printf("failed to validate register user")
 		httpUtils.WriteErrorResponse(w, http.StatusInternalServerError, errorWrapper.NewError(
 			errorWrapper.UserNameError, "длина имени пользователя должна быть больше 1",
@@ -39,8 +44,8 @@ func (h *AuthSubrouter) Register(w http.ResponseWriter, r *http.Request) {
 	if _, err := h.UsersService.CreateUser(ctx, entity.User{
 		Uid:       uid,
 		Email:     req.Email,
-		FirstName: nameSlice[0],
-		LastName:  nameSlice[1],
+		FirstName: firstName,
+		LastName:  lastName,
 	}); err != nil {
 		log.Printf("failed to create user %s in gw: %v", uid, err)
 		httpUtils.WriteErrorResponse(w, http.StatusInternalServerError, errorWrapper.NewError(
