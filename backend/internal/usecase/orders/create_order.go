c318b87920fb2ca19625afae42a7f460bfec33f6
Балобанов Владислав <vbalobanov@zunami.ru>
2024-11-15 15:26:48 +0300

    feat(ucOrders): refactor createOrder

--- backend/internal/usecase/orders/create_order.go
+++ backend/internal/usecase/orders/create_order.go
@@ -4,35 +4,20 @@ import (
 	"context"
 	"log"
 
+	"github.com/pkg/errors"
 	uuid "github.com/satori/go.uuid"
 	"github.com/uxsnap/fresh_market_shop/backend/internal/entity"
 )
 
 func (s *UseCaseOrders) CreateOrder(ctx context.Context, userUid uuid.UUID, productsCounts entity.ProductsCounts) (uuid.UUID, error) {
-	log.Printf("ucOrders.CreateOrder")
-
-	if err := s.productsCountRepository.CheckIfAllItemsExist(ctx, productsCounts); err != nil {
-		log.Printf("failed to validate order creation: %v", err)
-		return uuid.UUID{}, err
-	}
-
-	if err := s.productsCountRepository.UpdateCount(ctx, productsCounts); err != nil {
-		log.Printf("failed to update products count: %v", err)
-		return uuid.UUID{}, err
-	}
+	log.Printf("ucOrders.CreateOrder: user uid %s", userUid)
 
 	order := entity.Order{
 		UserUid: userUid,
 		Uid:     uuid.NewV4(),
 	}
 
-	if err := s.ordersRepository.CreateOrder(ctx, order); err != nil {
-		log.Printf("failed to create order: %v", err)
-		return uuid.UUID{}, err
-	}
-
 	orderProducts := make([]entity.OrderProducts, len(productsCounts.Products))
-
 	for ind, val := range productsCounts.Products {
 		orderProducts[ind] = entity.OrderProducts{
 			OrderUid:   order.Uid,
@@ -41,9 +26,30 @@ func (s *UseCaseOrders) CreateOrder(ctx context.Context, userUid uuid.UUID, prod
 		}
 	}
 
-	if err := s.orderProductsRepository.AddOrderProducts(ctx, orderProducts); err != nil {
-		log.Printf("failed to create order: %v", err)
-		return uuid.UUID{}, err
+	if err := s.txManager.NewPgTransaction().Execute(ctx, func(ctx context.Context) error {
+
+		if err := s.productsCountRepository.CheckIfAllItemsExist(ctx, productsCounts); err != nil {
+			log.Printf("failed to validate order creation: %v", err)
+			return err
+		}
+
+		if err := s.productsCountRepository.UpdateCount(ctx, productsCounts); err != nil {
+			log.Printf("failed to update products count: %v", err)
+			return err
+		}
+
+		if err := s.ordersRepository.CreateOrder(ctx, order); err != nil {
+			log.Printf("failed to create order: %v", err)
+			return err
+		}
+
+		if err := s.orderProductsRepository.AddOrderProducts(ctx, orderProducts); err != nil {
+			log.Printf("failed to create order: %v", err)
+			return err
+		}
+		return nil
+	}); err != nil {
+		return uuid.UUID{}, errors.WithStack(err)
 	}
 
 	return order.Uid, nil
