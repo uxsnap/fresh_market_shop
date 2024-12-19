c49a923c7f97aa720c84487fd7f5ae3c2b37a070
uxsnap <nuxxx7132@gmail.com>
2024-12-19 16:46:06 +0200

    update create_admin

--- backend/internal/client/services/auth/create_admin.go
+++ backend/internal/client/services/auth/create_admin.go
@@ -6,25 +6,27 @@ import (
 
 	uuid "github.com/satori/go.uuid"
 	"github.com/uxsnap/fresh_market_shop/backend/pkg/auth_v1"
+	"google.golang.org/grpc/metadata"
 )
 
 func (c *AuthClient) CreateAdmin(ctx context.Context, email string, password string, token string) (uuid.UUID, error) {
-	log.Printf("authClient.CreateAdmin: email %s", email)
+	log.Printf("authClient.CreateAdmin: email %s, token %s", email, token)
+
+	ctx = metadata.AppendToOutgoingContext(ctx, accessJwtKey, token)
 
 	resp, err := c.client.CreateAdmin(ctx, &auth_v1.AdminCreateRequest{
 		Email:    email,
 		Password: password,
-		Jwt:      token,
 	})
 	if err != nil {
-		log.Printf("failed to register user: %v", err)
+		log.Printf("failed to create admin user: %v", err)
 		return uuid.UUID{}, err
 	}
 
 	userUid, err := uuid.FromString(resp.Uid)
 
 	if err != nil {
-		log.Printf("register returns uncorrect uid: (%s), error: %v", resp.Uid, err)
+		log.Printf("create admin returns uncorrect uid: (%s), error: %v", resp.Uid, err)
 		return uuid.UUID{}, err
 	}
 
